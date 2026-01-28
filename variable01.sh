1752c5e7dd85cf1fada160599136cf66bb890205
Rajamohan4487 <rajamohan4487@gmail.com>
2026-01-28 19:36:26 +0530

    Shell Practise

---
+++ variable01.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1="Raja"
+PERSON2="Siva"
+
+echo " $PERSON1: Hi $PERSON2!"
+echo " $PERSON2: HI $PERSON1 how are you?"
+echo " $PERSON1: i am fine, how are you?"
+echo " $PERSON2: i am good $PERSON2, thanks for asking how about you."
